32e61d6321d691c6c53c2478f25cdd36c8aba864
Lainieeeee <yorin0820@gmail.com>
2024-09-25 23:23:38 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
 
 const router = createRouter({
   // history: createWebHistory(process.env.BASE_URL),
-  history: createWebHashHistory('/vue-portfolio/'), // ハッシュモードに変更
+  history: createWebHistory('/vue-portfolio/'),
   routes,
 
   // アンカーリンク機能追加
